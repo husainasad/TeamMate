0ca049531e48ee5a3100561b597728580cf113b5
husainasad <asad1999@gmail.com>
2024-05-19 16:55:12 -0700

    implemented standard naming conventions for urls

--- TeamMate_DJ/taskManager/urls.py
+++ TeamMate_DJ/taskManager/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.display_hello, name='default'),
     path('testing/', views.testing, name='testing'),
     path('tasks/', views.display_tasks, name='pending tasks'),
-    path('addTask/', views.add_task, name='add task'),
-    path('completedTasks/', views.completed_tasks, name='completed tasks'),
-    path('taskDetails/<int:id>', views.tasks_details, name='task details'),
+    path('tasks/new/', views.add_task, name='add task'),
+    path('tasks/completed/', views.completed_tasks, name='completed tasks'),
+    path('tasks/<int:id>', views.tasks_details, name='task details'),
 ]
