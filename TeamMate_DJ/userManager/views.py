9bdf5dd558c058bf23c09c61496a50449dce7c5f
husainasad <asad1999@gmail.com>
2024-06-05 02:01:56 -0700

    implemented password change | implemented user registration

--- TeamMate_DJ/userManager/views.py
+++ TeamMate_DJ/userManager/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
 def dashboard(request):
-    return render(request, "users/dashboard.html")  
+    return render(request, "users/dashboard.html")
+
+def register(request):
+    if request.method=='POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("dashboard"))
+    else:
+        return render(
+            request, "users/register.html",
+            {"form": CustomUserCreationForm}
+        )
